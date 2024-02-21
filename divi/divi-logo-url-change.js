57d9e9949ace3f9cae5532731cff2c052e6ce2f2
Faisal Ahammad <faisalahammad24@gmail.com>
2024-02-21 18:36:08 +0600

    Added - Change logo URL

---
+++ divi/divi-logo-url-change.js
@@ -0,0 +1,7 @@
+/**
+ * This is for default header, not global
+ * Change the example.com URL with your own
+ */
+jQuery(document).ready(function ($) {
+  $("#logo a").attr("href", "https://example.com");
+});
