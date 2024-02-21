843ed1e252eab4b877de2a96366511abfe3ab695
Faisal Ahammad <faisalahammad24@gmail.com>
2024-02-21 18:46:33 +0600

    Divi - Change logo img alt text added

---
+++ divi/divi-logo-img-alt-text.js
@@ -0,0 +1,9 @@
+/**
+ * Change the alt text from the following code
+ */
+
+jQuery(document).ready(function ($) {
+  setTimeout(function () {
+    $(".logo_container img").attr("alt", "New text goes here");
+  }, 10);
+});
