5a1ecaa48329e7a6758f38627998be6db34d0f07
Faisal Ahammad <faisalahammad24@gmail.com>
2024-04-12 03:20:46 +0600

    Minor fixes

--- gravity-forms/checbox-unique-items-selected.js
+++ gravity-forms/checbox-unique-items-selected.js
@@ -1,38 +1,26 @@
-document.ready(function () {
+jQuery(document).ready(function () {
   // replace the core-skills and secondary-skills with your own field class
-  $(
-    '.core-skills input[type="checkbox"], .secondary-skills input[type="checkbox"]'
-  ).change(function () {
+  $('.core-skills input[type="checkbox"], .secondary-skills input[type="checkbox"]').change(function () {
     var selectedCoreSkills = $('.core-skills input[type="checkbox"]:checked')
       .map(function () {
         return $(this).val();
       })
       .get();
 
-    var selectedSecondarySkills = $(
-      '.secondary-skills input[type="checkbox"]:checked'
-    )
+    var selectedSecondarySkills = $('.secondary-skills input[type="checkbox"]:checked')
       .map(function () {
         return $(this).val();
       })
       .get();
 
-    $(
-      '.core-skills input[type="checkbox"], .secondary-skills input[type="checkbox"]'
-    ).prop("disabled", false);
+    $('.core-skills input[type="checkbox"], .secondary-skills input[type="checkbox"]').prop("disabled", false);
 
     selectedCoreSkills.forEach(function (skill) {
-      $('.secondary-skills input[type="checkbox"][value="' + skill + '"]').prop(
-        "disabled",
-        true
-      );
+      $('.secondary-skills input[type="checkbox"][value="' + skill + '"]').prop("disabled", true);
     });
 
     selectedSecondarySkills.forEach(function (skill) {
-      $('.core-skills input[type="checkbox"][value="' + skill + '"]').prop(
-        "disabled",
-        true
-      );
+      $('.core-skills input[type="checkbox"][value="' + skill + '"]').prop("disabled", true);
     });
   });
 });
