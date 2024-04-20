4142b5a53290b4d508ab955b75a07652d2e4e6d6
Faisal Ahammad <faisalahammad24@gmail.com>
2024-04-20 16:00:18 +0600

    Divi - Display shipping fields

---
+++ divi/divi-display-shipping-fields.js
@@ -0,0 +1,8 @@
+/**
+ * Divi - Display shipping fields
+ */
+jQuery(document).ready(function () {
+  setTimeout(function () {
+    jQuery(".woocommerce-checkout .et_pb_module.et_pb_wc_checkout_shipping .shipping_address").removeAttr("style");
+  }, 500);
+});
