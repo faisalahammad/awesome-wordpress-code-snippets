db4a4e3122596003d950151ed3d73d5238e32988
Faisal Ahammad <faisalahammad24@gmail.com>
2024-04-22 04:15:54 +0600

    Gravity Forms - Replace Required with Asterisk

---
+++ gravity-forms/replace-required-with-asterisk.js
@@ -0,0 +1,8 @@
+/**
+ * Gravity Forms - Replace Required with Asterisk
+ * Source: https://community.gravityforms.com/t/customization-term-required/15851
+ */
+
+jQuery(document).ready(function ($) {
+  $(".gfield_required.gfield_required_text").text("*");
+});
