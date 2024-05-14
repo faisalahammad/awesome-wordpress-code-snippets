2f2c9a3b391b69b01f05cb80e456cd7be165db9c
Faisal Ahammad <faisalahammad24@gmail.com>
2024-05-14 20:54:01 +0600

    Image scaling threshold increase

---
+++ common/image-scaling-threshold-increase.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Image scaling threshold increase
+ * Source: https: //smartwp.com/large-image-scaling-wordpress/
+ */
+
+/**
+ * @param  $threshold
+ * @return int
+ */
+function smartwp_big_image_size_threshold( $threshold )
+{
+ return 4000;
+}
+
+add_filter( 'big_image_size_threshold', 'smartwp_big_image_size_threshold', 999, 1 );
