44593d0c181534a7ace1db8c35ea589a7d7d8efc
Faisal Ahammad <faisalahammad24@gmail.com>
2024-06-11 16:05:11 +0600

    Ninja Forms - Change Field Type to URL

---
+++ ninja-forms/change-field-type-to-url.js
@@ -0,0 +1,10 @@
+/**
+ * Ninja Forms - Change Field Type to URL
+ * @author Faisal Ahammad
+ */
+
+jQuery(document).ready(function ($) {
+  setTimeout(function () {
+    $("input.websiteurl").attr("type", "url");
+  }, 500);
+});
