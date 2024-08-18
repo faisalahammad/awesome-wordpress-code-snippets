7cec6a88966fd8447fedc850a719c6a34cbe8c30
Faisal Ahammad <faisalahammad24@gmail.com>
2024-08-19 05:51:21 +0600

    Ninja Forms - Allow Future Dates in Advanced Datepicker add-on

---
+++ ninja-forms/allow-future-dates-advanced-datepicker.js
@@ -0,0 +1,35 @@
+/**
+ * Ninja Forms - Allow Future Dates in Advanced Datepicker add-on
+ * @author Faisal Ahammad
+ */
+
+jQuery(document).ready(function ($) {
+  function initCustomDatePicker() {
+    var customDatePicker = Marionette.Object.extend({
+      initialize: function () {
+        this.listenTo(Backbone.Radio.channel("flatpickr"), "init", this.modifyDatepicker);
+      },
+      modifyDatepicker: function (dateObject, fieldModel) {
+        var currentDate = new Date();
+        currentDate.setDate(currentDate.getDate() + 1);
+        dateObject.set("minDate", currentDate);
+      },
+    });
+
+    new customDatePicker();
+  }
+
+  // Form ID (185) to apply the code on a specific form
+  if ($("#nf-form-185-cont").length) {
+    initCustomDatePicker();
+  } else {
+    var observer = new MutationObserver(function (mutations, observerInstance) {
+      if ($("#nf-form-185-cont").length) {
+        initCustomDatePicker();
+        observerInstance.disconnect(); // Stop observing once the form is found
+      }
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+  }
+});
