0b084eec931eb200f7f8d8ee8102a1ff67b1ae51
Faisal Ahammad <faisalahammad24@gmail.com>
2024-08-26 12:02:51 +0600

    Improved the code

--- ninja-forms/required-field-text-broken-code-fix.php
+++ ninja-forms/required-field-text-broken-code-fix.php
@@ -2,9 +2,9 @@
 /**
  * Ninja Forms - Required Field Text Broken Code Fix
  * @package Ninja Forms
+ *
+ * @author Faisal Ahammad
  */
-
-add_filter( 'ninja_forms_i18n_front_end', 'fix_ninja_forms_i18n_front_end' );
 
 /**
  * @param  $strings
@@ -12,17 +12,24 @@ add_filter( 'ninja_forms_i18n_front_end', 'fix_ninja_forms_i18n_front_end' );
  */
 function fix_ninja_forms_i18n_front_end( $strings )
 {
- $strings[ 'fieldsMarkedRequired' ] = 'Fields marked with an <span class="ninja-forms-req-symbol">*</span> are required';
+  $strings[ 'fieldsMarkedRequired' ] = 'Fields marked with an <span class="ninja-forms-req-symbol">*</span> are required';
 
- return $strings;
+  return $strings;
 }
+
+add_filter( 'ninja_forms_i18n_front_end', 'fix_ninja_forms_i18n_front_end' );
 
 // Alternative Code
-function decode_ninja_forms_display_form_settings($settings, $form_id)
+/**
+ * @param  $settings
+ * @param  $form_id
+ * @return mixed
+ */
+function decode_ninja_forms_display_form_settings( $settings, $form_id )
 {
-	$settings['fieldsMarkedRequired'] = html_entity_decode($settings['fieldsMarkedRequired']);
+  $settings[ 'fieldsMarkedRequired' ] = html_entity_decode( $settings[ 'fieldsMarkedRequired' ] );
 
-	return $settings;
+  return $settings;
 }
 
-add_filter('ninja_forms_display_form_settings', 'decode_ninja_forms_display_form_settings', 10, 2);
+add_filter( 'ninja_forms_display_form_settings', 'decode_ninja_forms_display_form_settings', 10, 2 );
