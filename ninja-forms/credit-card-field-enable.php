9f38534d7de7be0fc4d6f5f282db24eac51ded58
Faisal Ahammad <faisalahammad24@gmail.com>
2025-01-06 09:44:50 +0600

    Ninja Forms - Enable Credit Card Fields

---
+++ ninja-forms/credit-card-field-enable.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Ninja Forms - Enable Credit Card Fields
+ * @author Faisal Ahammad <me@faisalahammad.com>
+ */
+
+add_filter( 'ninja_forms_enable_credit_card_fields', '__return_true' );
