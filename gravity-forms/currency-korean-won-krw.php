de42aaa01c65920957426a64058dc3a3a25c7deb
Faisal Ahammad <faisalahammad24@gmail.com>
2025-01-20 20:48:56 +0600

    Add Korean Won (KRW) currency to Gravity Forms

---
+++ gravity-forms/currency-korean-won-krw.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Add Korean Won (KRW) currency to Gravity Forms
+ * @author Faisal Ahammad <me@faisalahammad.com>
+ */
+
+add_filter( 'gform_currencies', 'add_krw_currency' );
+
+/**
+ * @param  $currencies
+ * @return mixed
+ */
+function add_krw_currency( $currencies )
+{
+ $currencies[ 'KRW' ] = array(
+  'name'               => __( 'Korean Won', 'gravityforms' ),
+  'code'               => 'KRW',
+  'symbol_left'        => '₩',
+  'symbol_right'       => '',
+  'symbol_padding'     => ' ',
+  'thousand_separator' => ',',
+  'decimal_separator'  => '.',
+  'decimals'           => 0,
+ );
+
+ return $currencies;
+}
