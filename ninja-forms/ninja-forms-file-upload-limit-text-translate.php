846d8ec65bde932e58a29831a88d8b14c9e96856
Faisal Ahammad <faisalahammad24@gmail.com>
2025-07-14 21:31:08 +0600

    Add custom translation for Ninja Forms file upload limit
    Introduces a filter to customize the 'Max x files are allowed' message in Ninja Forms File Uploads, providing a German translation for the file limit message.

---
+++ ninja-forms/ninja-forms-file-upload-limit-text-translate.php
@@ -0,0 +1,15 @@
+/**
+ * Customize the "Max x files are allowed" message in Ninja Forms File Uploads
+ * @author Faisal Ahammad <me@faisalahammad.com>
+ */
+
+<?php
+// Customize the "Max x files are allowed" message in Ninja Forms File Uploads
+add_filter( 'ninja_forms_uploads_js_strings', 'custom_ninja_forms_uploads_js_strings' );
+
+function custom_ninja_forms_uploads_js_strings( $strings ) {
+    // Change default message to German: "Max %n Dateien sind erlaubt"
+    $strings['file_limit'] = __( 'Max %n Dateien sind erlaubt', 'ninja-forms-uploads' );
+
+    return $strings;
+}
